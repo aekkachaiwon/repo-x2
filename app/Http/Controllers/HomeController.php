5395a754766f45cb836dac3fce14fa94379b83aa
Aekkachai Wongkhsame <filmtory2541@gmail.com>
2020-08-16 12:47:22 +0700

    initial commit

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+    public function index()
+    {
+        $data = [
+            "name" => "Bundit Nuntates",
+            "age" => 30,
+        ];
+        return view("welcome", $data);
+    }
+
+}
